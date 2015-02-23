99928de1f381a7969b8d5a2bb95ee04534c7ed26
Matheus S. Fontana <msfontana.dev@gmail.com>
2015-02-23 01:24:57 -0600

    Update tecjson.js

--- tecjson.js
+++ tecjson.js
@@ -1,10 +1,12 @@
 {
   "friends":[
-        {"name":"Wiliam Aguirre","hobby":"Play soccer, or at least die trying to do it","age":"23","phone":"9999 9999","address":"Canoas, Rio Grande do Sul, Brazil"},
+        {"name":"Dilma Rousseff","hobby":"Drive Brazil downwards","age":"67","phone":"171 171 13","address":"Brasilia, Distrito Federal, Brazil"},
+        {"name":"Ronaldinho Gaucho","hobby":"Party hard. And sometimes show his football skills","age":"34","phone":"10 49 80 11","address":"Santiago de Querétaro, Querétaro, Spain"},  
         {"name":"Barack Obama","hobby":"Make fun of Vladimir Putin","age":"53","phone":"No one knows","address": "Washington, District of Columbia, USA"},  
         {"name":"Lionel Messi","hobby":"Score goals against every goalkeeper he plays against","age":"28","phone":"10 10 10 10","address":"Barcelona, Catalunya, Spain"}, 
-        {"name":"Mauro Reis","hobby":"Use BackTrack Linux to have some fun inside someone else's server...","age":"30","phone":"+55 51 9999 8888","address":"Porto Alegre, Rio Grande do Sul, Brazil"},  {"name":"Linus Torvalds","hobby":"Compile his linux kernel every single day","age":"45","phone":"01 01 01 01","address":"Portland, Oregon, USA"},  
-        {"name":"Chico Dinero","hobby":"Create not understandable rap lyrics without any rhyme","age":"15","phone":"9999 9999","address":"Mexico City, Distrito Federal, Mexico"},
+        {"name":"Linus Torvalds","hobby":"Compile his linux kernel every single day","age":"45","phone":"01 01 01 01","address":"Portland, Oregon, USA"},
+        {"name":"Chicharito","hobby":"Warm Real Madrid's bench","age":"26","phone":"14 14 14 14","address":"Madrid, Comunidad de Madrid, Spain"},  
+        {"name":"MC Dinero","hobby":"Create not understandable rap lyrics without any rhyme","age":"15","phone":"9999 9999","address":"Mexico City, Distrito Federal, Mexico"},
         {"name":"Arjen Robben","hobby":"Fake penalties and fouls, mainly when he is losing a football game","age":"31","phone":"0000 0000","address":"Munich, Bayern, Germany"}
   ]
 }
