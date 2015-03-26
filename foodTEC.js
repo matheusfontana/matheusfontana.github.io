01612f926182f3c11ce3f63c7024886b74157309
Matheus S. Fontana <msfontana.dev@gmail.com>
2015-03-26 14:21:17 -0600

    Update foodTEC.js

--- foodTEC.js
+++ foodTEC.js
@@ -4,64 +4,9 @@
               "name":"Subway",
               "lat": "20.734540",
               "lon": "-103.457680",
-              
+              "img": "https://irs1.4sqi.net/img/general/width960/39460857_lC-KOZZMdXPa52Z7KCGxMxk0i5AcbAe0RCcmZlxM-m0.jpg",
               "sell": "Customer made sandwiches"
             },
-            {
-              "name":"Cafetería",
-              "lat": "20.7329919",
-              "lon": "-103.4546382",
-              
-              "sell": "Meals"
-            },
-            {
-              "name":"Starbucks",
-              "lat": "20.734653",
-              "lon": "-103.4551252",
-              
-              "sell": "Coffee and snacks"
-            },
-            {
-              "name":"Juvie Jus",
-              "lat": "20.734642",
-              "lon": "-103.457674",
-              
-              "sell": "Healthy food"
-              
-            },
-            {
-              "name":"Oxxo",
-              "lat": "20.7341744",
-              "lon": "-103.4561874",
-              
-              "sell": "Food, snacks, hygiene and cleaning products "
-            },
-            {
-              "name":"Guich - Cyber Plaza",
-              "lat": "20.734857",
-              "lon": "-103.455709",
-              
-              "sell": "Meals and snacks"
-            },
-            {
-              "name":"Yum-Yum Snack - Cyber Plaza",
-              "lat": "20.735028",
-              "lon": "-103.455741",
-              
-              "sell": "Meals and snacks"
-            },
-            {
-              "name":"La Selva",
-              "lat": "20.735135",
-              "lon": "-103.455707",
-              
-              "sell": "Meals and snacks"
-            },    
-            {
-              "name":"Yum-Yum Snack - Explanada Arquitectura",
-              "lat": "20.734481",
-              "lon": "-103.457693",
-              "sell": "Meals and snacks"
-            }              
+                     
   ]
 }
