48693af70ada8a52eda15fdb569e58de0f501b3f
Matheus S. Fontana <msfontana.dev@gmail.com>
2015-03-26 01:54:29 -0600

    Update foodTEC.js

--- foodTEC.js
+++ foodTEC.js
@@ -4,63 +4,63 @@
               "name":"Subway",
               "lat": "20.734540",
               "lon": "-103.457680",
-              "img": "https://irs1.4sqi.net/img/general/width960/39460857_lC-KOZZMdXPa52Z7KCGxMxk0i5AcbAe0RCcmZlxM-m0.jpg",
+              
               "sell": "Customer made sandwiches"
             },
             {
               "name":"Cafetería",
               "lat": "20.7329919",
               "lon": "-103.4546382",
-              "img": "https://irs0.4sqi.net/img/general/width960/40332862_9HmGWHDC_t4clAiq_Y9stVeUbL9WIYgWkIX16nCaMqs.jpg",
+              
               "sell": "Meals"
             },
             {
               "name":"Starbucks",
               "lat": "20.734653",
               "lon": "-103.4551252",
-              "img": "https://irs1.4sqi.net/img/general/width960/34ZE0GN2F2R4WFSCOSB2DKYW1IY3EGG2RGIHPZKFU4MIJPVZ.jpg",
+              
               "sell": "Coffee and snacks"
             },
             {
               "name":"Juvie Jus",
               "lat": "20.734642",
               "lon": "-103.457674",
-              "img": "https://irs2.4sqi.net/img/general/width960/4052139_N0sFIJtnK_jjzjY4_dz_rCkBXA638IuqJsdjA3n-jkY.jpg",
+              
               "sell": "Healthy food"
+              
             },
             {
               "name":"Oxxo",
               "lat": "20.7341744",
               "lon": "-103.4561874",
-              "img": "https://irs0.4sqi.net/img/general/width960/43799951_j7d3hui0Dm_3XGbePkMoiSqDA_WJPA79c4F-NT3w_E0.jpg",
+              
               "sell": "Food, snacks, hygiene and cleaning products "
             },
             {
               "name":"Guich - Cyber Plaza",
               "lat": "20.734857",
               "lon": "-103.455709",
-              "img" "https://irs1.4sqi.net/img/general/width960/529797_Im0_XUu4rWj6jUK_aPno0H7JjOgbxuFNQYff9sONCjo.jpg",
+              
               "sell": "Meals and snacks"
             },
             {
               "name":"Yum-Yum Snack - Cyber Plaza",
               "lat": "20.735028",
               "lon": "-103.455741",
-              "img": "https://irs0.4sqi.net/img/general/600x600/17458266_eRreB9TIm_fRYokifFIJikyYy0VOAkoP7cnlf4OCgiA.jpg",
+              
               "sell": "Meals and snacks"
             },
             {
               "name":"La Selva",
               "lat": "20.735135",
               "lon": "-103.455707",
-              "img": "https://irs3.4sqi.net/img/general/width960/cbf2GQ3qEX0H0xfSDYgruHl1Y-DadcKuo0Pr2QrVrtw.jpg",
+              
               "sell": "Meals and snacks"
             },    
             {
               "name":"Yum-Yum Snack - Explanada Arquitectura",
               "lat": "20.734481",
               "lon": "-103.457693",
-              "img": "https://irs0.4sqi.net/img/general/600x600/17458266_eRreB9TIm_fRYokifFIJikyYy0VOAkoP7cnlf4OCgiA.jpg",
               "sell": "Meals and snacks"
             }              
   ]
