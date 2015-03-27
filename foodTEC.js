e41e4d9292c3ccc6894ac26ce2876d85e5229b0d
Matheus S. Fontana <msfontana.dev@gmail.com>
2015-03-26 18:52:18 -0600

    Update foodTEC.js

--- foodTEC.js
+++ foodTEC.js
@@ -14,6 +14,28 @@
               "img": "https://irs0.4sqi.net/img/general/width960/40332862_9HmGWHDC_t4clAiq_Y9stVeUbL9WIYgWkIX16nCaMqs.jpg",
               "sell": "Meals"
             },
+            {
+              "name":"Starbucks",
+              "lat": "20.734653",
+              "lon": "-103.4551252",
+              "img": "https://irs1.4sqi.net/img/general/width960/34ZE0GN2F2R4WFSCOSB2DKYW1IY3EGG2RGIHPZKFU4MIJPVZ.jpg",
+              "sell": "Coffee and snacks"
+            },
+            {
+              "name":"Juvie Jus",
+              "lat": "20.734642",
+              "lon": "-103.457674",
+              "img": "https://irs2.4sqi.net/img/general/width960/4052139_N0sFIJtnK_jjzjY4_dz_rCkBXA638IuqJsdjA3n-jkY.jpg",
+              "sell": "Healthy food"
+              
+            },
+            {
+              "name":"Oxxo",
+              "lat": "20.7341744",
+              "lon": "-103.4561874",
+              "img": "https://irs0.4sqi.net/img/general/width960/43799951_j7d3hui0Dm_3XGbePkMoiSqDA_WJPA79c4F-NT3w_E0.jpg",
+              "sell": "Food, snacks, hygiene and cleaning products "
+            },
             
   ]
 }
